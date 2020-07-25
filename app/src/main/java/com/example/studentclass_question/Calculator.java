cdd922c582765a1bb4033464dbc29381b2742b72
LMY <lmun768@naver.com>
2020-07-25 21:12:13 +0900

    [ADD] BASE

---
+++ app/src/main/java/com/example/studentclass_question/Calculator.java
@@ -0,0 +1,19 @@
+package com.example.studentclass_question;
+
+public class Calculator {
+    public float add(float firstValue, float secondValue){
+        return firstValue + secondValue;
+    }
+
+    public float miuns(float firstValue, float secondValue){
+        return firstValue - secondValue;
+    }
+
+    public float multiplication(float firstValue, float secondValue){
+        return firstValue * secondValue;
+    }
+
+    public float division(float firstValue, float secondValue){
+        return firstValue / secondValue;
+    }
+}
