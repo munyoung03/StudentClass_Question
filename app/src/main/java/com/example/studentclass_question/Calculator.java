edd3e3240aabb6d3eb36889506c3503fce1484d7
LMY <lmun768@naver.com>
2020-07-27 20:23:21 +0900

    END

--- app/src/main/java/com/example/studentclass_question/Calculator.java
+++ app/src/main/java/com/example/studentclass_question/Calculator.java
@@ -2,18 +2,18 @@ package com.example.studentclass_question;
 
 public class Calculator {
     public float add(float firstValue, float secondValue){
-        return firstValue + secondValue;
+        //firstValue와 secondValue를 더하여 리턴하는 코드
     }
 
     public float miuns(float firstValue, float secondValue){
-        return firstValue - secondValue;
+        //firstValue와 secondValue를 뺄셈하여 리턴하는 코드
     }
 
     public float multiplication(float firstValue, float secondValue){
-        return firstValue * secondValue;
+        //firstValue와 secondValue를 곱하여 리턴하는 코드
     }
 
     public float division(float firstValue, float secondValue){
-        return firstValue / secondValue;
+        //firstValue와 secondValue를 나누어 리턴하는 코드
     }
 }
--- app/src/main/java/com/example/studentclass_question/MainActivity.kt
+++ app/src/main/java/com/example/studentclass_question/MainActivity.kt
@@ -4,8 +4,59 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    //버튼 정의
+
+
+    //텍스트뷰 정의
+
+
+    //Edit텍스트 정의
+
+
+    //계산을 하기위한 edittext의 값을 저장시킬 변수
+
+
+    //Calculator클래스를 정의할 변수
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //xml의 버튼과 액티비티 연결
+
+
+        //xml의 텍스트뷰와 액티비티 연결
+
+
+        //xml의 Edit텍스트와 액티비티 연결
+
+
+        //Calculator 객체 생성
+
+
+        //addBtn 클릭이벤트{
+        // Edit텍스트값을 getText를 통하여 값을 가져옴
+        // calculator.add 함수를 호출하여 리턴받은값을 텍스트뷰에 저장
+        //}
+
+
+        //minusBtn 클릭이벤트{
+        // Edit텍스트값을 getText를 통하여 값을 가져옴
+        // calculator.minus 함수를 호출하여 리턴받은값을 텍스트뷰에 저장
+        //}
+
+
+        //multiplicationBtn 클릭이벤트{
+        // Edit텍스트값을 getText를 통하여 값을 가져옴
+        // calculator.multiplication 함수를 호출하여 리턴받은값을 텍스트뷰에 저장
+        //}
+
+
+        //divisionBtn 클릭이벤트{
+        // Edit텍스트값을 getText를 통하여 값을 가져옴
+        // calculator.division 함수를 호출하여 리턴받은값을 텍스트뷰에 저장
+        //}
     }
 }
